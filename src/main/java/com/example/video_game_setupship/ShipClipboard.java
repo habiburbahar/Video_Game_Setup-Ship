3f37b64a6a991b30ba85d4ab537f9db1effd127e
Habibur Rahman Bahar <55600689+habiburbahar@users.noreply.github.com>
2022-01-03 01:26:55 -0600

    Manuel clipboard class have been added

--- src/main/java/com/example/video_game_setupship/ShipClipboard.java
+++ src/main/java/com/example/video_game_setupship/ShipClipboard.java
@@ -1,8 +1,34 @@
 package com.example.video_game_setupship;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ShipClipboard {
     List<Groupable> items;
+
+    public ShipClipboard() {
+        items = new ArrayList<>();
+    }
+
+    public void copy(List<Groupable> gs) {
+        items.clear();
+        gs.forEach(g -> items.add(g.duplicate()));
+    }
+
+    public List<Groupable> get() {
+        List<Groupable> copy = new ArrayList<Groupable>();
+        items.forEach(item -> copy.add(item.duplicate()));
+        return copy;
+    }
+
+    public String describeClipboard() {
+        if (items.size() > 1) {
+            return items.size() + " items";
+        } else if (items.size() == 1) {
+            return "1 item";
+        } else {
+            return "empty";
+        }
+    }
 
 }
